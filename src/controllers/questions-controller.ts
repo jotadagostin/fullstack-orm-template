a12f598580bf7e8ad4b364f20f50e62e4a3906b9
João Paulo Dagostin <107184368+jotadagostin@users.noreply.github.com>
2025-08-20 15:31:24 +0200

    table

--- src/controllers/questions-controller.ts
+++ src/controllers/questions-controller.ts
@@ -1,12 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-
+import { prisma } from "@/prisma";
 class QuestionsController {
   async index(request: Request, response: Response) {
     return response.json();
   }
 
   async create(request: Request, response: Response) {
-    return response.status(201).json({ message: "ok" });
+    const { title, content, user_id } = request.body;
+
+    await prisma.questions.create({
+      data: { title, content, user: user_id },
+    });
+
+    return response.status(201).json();
   }
 
   async update(request: Request, response: Response) {
